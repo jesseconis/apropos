62fad2a041112d688b9d9f1c45eb68bd53713f14
Jesse <dev@jesseconis.io>
2025-07-02 00:15:56 +1000

    feat(ui):✨ mantine AppShell

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createTheme, MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+// ‼️ import tiptap styles after core package styles
+import '@mantine/tiptap/styles.css';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 );
