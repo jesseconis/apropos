3e4151e13ffa44b522d81618056f843cde212ad0
Jesse <dev@jesseconis.io>
2025-07-02 03:41:39 +1000

    wip(ui):🚧 theme & Layout props

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createTheme, MantineProvider } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
+import { DefaultTheme  } from './theme.tsx';
 import '@mantine/core/styles.css';
 // ‼️ import tiptap styles after core package styles
 import '@mantine/tiptap/styles.css';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider
+    theme={{
+      ...DefaultTheme,
+    }}
+    >
       <App />
     </MantineProvider>
   </React.StrictMode>,
---
+++ src/theme.tsx
@@ -0,0 +1,6 @@
+import { createTheme } from '@mantine/core';
+
+export const DefaultTheme = createTheme({
+  primaryColor: 'blue',
+  defaultRadius: 0,
+});
